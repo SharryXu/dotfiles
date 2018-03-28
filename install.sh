07a7fa930544924c7aa87be59729090688b4c054
Sharry Xu <sharry.r.xu@gmail.com>
2018-03-28 14:43:57 +0800

    Configure travis.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # This program is used to install the entire repository.
 
@@ -9,20 +9,13 @@ function main() {
   if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
     RED="$(tput setaf 1)"
     GREEN="$(tput setaf 2)"
-    YELLOW="$(tput setaf 3)"
-    BLUE="$(tput setaf 4)"
-    BOLD="$(tput bold)"
     NORMAL="$(tput sgr0)"
   else
     RED=""
     GREEN=""
-    YELLOW=""
-    BLUE=""
-    BOLD=""
     NORMAL=""
   fi
 
-  printf "${GREEN}"
   echo '  _____   _____   _____   _____   _   _       _____   _____   '
   echo ' |  _  \ /  _  \ |_   _| |  ___| | | | |     | ____| /  ___/  '
   echo ' | | | | | | | |   | |   | |__   | | | |     | |__   | |___   '
@@ -32,8 +25,7 @@ function main() {
   echo ''
   echo 'If you have any question, feel free to email: sharry.r.xu@gmail.com.'
 
-  printf "\n${BLUE}Installing dotfiles to current folder...${NORMAL}\n"
-  printf "${NORMAL}"
+  echo -e '\nInstalling dotfiles to current folder...\n'
 
   hash git >/dev/null 2>&1 || {
     echo "Please install git."
@@ -43,21 +35,18 @@ function main() {
   local_folder="$(realpath .)/dotfiles"
   git clone --depth 1 https://github.com/SharryXu/dotfiles "$local_folder"
 
-  dotfile="$local_folder/bin/dotfile"
-  "$dotfile" -i "$local_folder"
+  # Install custom commands
+  chmod +x "$local_folder"/bin/install-custom-commands && "$local_folder"/bin/install-custom-commands "$local_folder"
 
-  if [[ $? == 0 ]]; then
-    printf "${GREEN}"
-    echo '\nInstallation finished successfully.'
-    printf "${NORMAL}"
+  if "$local_folder/bin/dotfile" -i "$local_folder"; then
+    printf "%s" "${GREEN}"
+    echo -e '\nInstallation finished successfully.'
+    printf "%s" "${NORMAL}"
   else
-    printf "${RED}"
-    echo '\nSomething wrong happended inside the installation.'
-    printf "${NORMAL}"
+    printf "%s" "${RED}"
+    echo -e '\nSomething wrong happended inside the installation.'
+    printf "%s" "${NORMAL}"
   fi
 }
 
-set -e
-
 main
-
