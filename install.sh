179299bc4f02555068baaa1db4686c23a9549d9f
Sharry Xu <sharry.r.xu@gmail.com>
2018-04-15 14:04:59 +0800

    Remove unused commands and use dotfile -u for updating configurations.

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # This program is used to install the entire repository.
 
 function main() {
+  local_folder="$(pwd)"/dotfiles
+
   echo '  _____   _____   _____   _____   _   _       _____   _____   '
   echo ' |  _  \ /  _  \ |_   _| |  ___| | | | |     | ____| /  ___/  '
   echo ' | | | | | | | |   | |   | |__   | | | |     | |__   | |___   '
@@ -11,24 +13,18 @@ function main() {
   echo ' |_____/ \_____/   |_|   |_|     |_| |_____| |_____| /_____/  '
   echo ''
   echo 'If you have any question, feel free to email: sharry.r.xu@gmail.com.'
-  echo ''
-  echo "Installing dotfiles to $(pwd)..."
 
-  hash git >/dev/null 2>&1 || {
+  if ! which git 1>/dev/null 2>&1; then
     echo "Please install git."
     exit 1
-  }
-
-  local_folder="$(realpath .)/dotfiles"
-  git clone --depth 1 https://github.com/SharryXu/dotfiles "$local_folder"
-
-  # Install custom commands
-  chmod +x "$local_folder"/bin/install-custom-commands && "$local_folder"/bin/install-custom-commands "$local_folder"
-
-  if $TRAVIS; then
-    /bin/bash -x "$local_folder"/bin/dotfile -n -i "$local_folder"
   else
-    "$local_folder"/bin/dotfile -i "$local_folder"
+    git clone --depth 1 https://github.com/SharryXu/dotfiles "$local_folder"
+
+    if $TRAVIS; then
+      /bin/bash -x "$local_folder"/bin/dotfile -i "$local_folder"
+    else
+      "$local_folder"/bin/dotfile -i "$local_folder"
+    fi
   fi
 }
 
